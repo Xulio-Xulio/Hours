3277cd68827e1db669d73e83824bf262b11daca8
Xulio-Xulio <61099971+Xulio-Xulio@users.noreply.github.com>
2020-03-04 23:13:57 -0300

    Finally

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function load() {
     var msg = document.querySelector(`div#msg`)
-    var photo = document.querySelector(`div#photo`) 
+    var photo = document.querySelector(`#photo`) 
     var date = new Date()
     var hour = date.getHours()
-    msg.innerHTML = `Now it's ${hour} hours <p>` 
+    msg.innerHTML = `<p> Now it's ${hour} hours <p/>` 
     
     if (hour >= 0 && hour < 12) {
-        img.scr = `day.jpg`
+        photo.src = `day.jpg`
         document.body.style.background = `#e2cd9f`
         msg.innerHTML += `<strong/>Good Morning<strong/>`
-    } else if (hour >= 0 && hour < 12) {
-        img.scr = `afternoon.jpg`
+    } else if (hour >= 12 && hour < 18) {
+        photo.src = `afternoon.jpg`
         document.body.style.background = `#b9846f`
         msg.innerHTML += `<strong/>Good Afternoon<strong/>`
     } else {
-        img.scr = `night.jpg`
+        photo.src = `night.jpg`
         document.body.style.background = `#515154`
         msg.innerHTML += `<strong/>Good Evening<strong/>`
     }
